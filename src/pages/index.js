44928dcba9108dbe98843ad0d5fd5d30e516aa44
wajahataz <54248569+wajahatabbas27@users.noreply.github.com>
2021-05-02 05:47:18 +0500

    step 17 completed

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,20 @@
-import React from "react"
+import React, { useEffect, useState } from "react"
 
 export default function Home() {
-  return <div>Hello world!</div>
+
+  const [data, setData] = useState("");
+
+  useEffect(() => {
+    console.log("UseEffect Called");
+
+    const functionCall = async () => {
+      const response = await fetch('/.netlify/functions/hello');
+      const tempData = await response.json();
+      setData(tempData);
+    }
+
+    functionCall();
+  }, []);
+
+  return <div>{data.message}</div>
 }
